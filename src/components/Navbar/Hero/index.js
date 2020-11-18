6d7cab57f82b422584b97f5c97735a47f0667711
RafaS92 <manrvaldez..92@gmail.com>
2020-11-18 16:10:51 -0600

    hero section completed

--- src/components/Navbar/Hero/index.js
+++ src/components/Navbar/Hero/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Navbar from '..'
-import {HeroContainer} from './HeroElements'
+import {HeroContainer,HeroContent,HeroItems,HeroH1,HeroP,HeroBtn} from './HeroElements'
 
 function Hero() {
     return (
         <div>
             <HeroContainer>
                 <Navbar />
+                <HeroContent>
+                    <HeroItems>
+                        <HeroH1>
+                            Greatest Burger Ever
+                        </HeroH1>
+                        <HeroP>Ready in 60 seconds</HeroP>
+                        <HeroBtn>Place Order</HeroBtn>
+                    </HeroItems>
+                </HeroContent>
             </HeroContainer>
         </div>
     )
