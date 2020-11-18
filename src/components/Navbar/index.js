4b9ecabe4b76f1ecf0e8a0f9384f0511a1e47596
RafaS92 <manrvaldez..92@gmail.com>
2020-11-18 14:24:39 -0600

    first components

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Nav, NavLink,NavIcon, Bars} from "./NavbarElements"
+
+export default function Navbar() {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">Burguers</NavLink>
+                <NavIcon>
+                
+                    <p>Menu</p>
+                    <Bars />
+                
+                </NavIcon>
+            </Nav>
+        </>
+    )
+}
+
+
