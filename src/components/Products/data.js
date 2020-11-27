d2fc4a8a2baa98d79634df5d1ff7b746ed4a4471
RafaS92 <manrvaldez..92@gmail.com>
2020-11-27 16:47:56 -0600

    products container created

---
+++ src/components/Products/data.js
@@ -0,0 +1,31 @@
+export const productData = [
+  {
+    img:
+      "https://www.cidadeoferta.com.br/storage/offers/9512-hamb_oferta7_imagem2.jpg",
+    alt: "Burger",
+    name: "Australasia",
+    desc:
+      "Popular regional hamburger, includes canned beetroot, pineapple and a fried egg.",
+    price: $12,
+    button: "Add to Cart",
+  },
+  {
+    img:
+      "https://i.pinimg.com/736x/41/a2/f6/41a2f6e20dd9f80a8cedc4324afe40ef.jpg",
+    alt: "Burger",
+    name: "Bacon",
+    desc: "Hamburger with bacon and cheese.",
+    price: "$13.99",
+    button: "Add to Cart",
+  },
+  {
+    img:
+      "https://portalbeiju.com.br/wp-content/uploads/2018/05/hamburguer-caseiro.jpg",
+    alt: "Burger",
+    name: "Barbecue",
+    desc:
+      "	Prepared with ground beef, mixed with onions and barbecue sauce, and then grilled.",
+    price: "$16.99",
+    button: "Add to Cart",
+  },
+];
