cee83d62e2bb1691ae11ff1d9d8c668eefbfed96
RafaS92 <manrvaldez..92@gmail.com>
2020-11-27 16:30:42 -0600

    sidebar create

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,24 +1,38 @@
-import React from 'react'
-import Navbar from '../Navbar'
-import {HeroContainer,HeroContent,HeroItems,HeroH1,HeroP,HeroBtn} from './HeroElements'
+import React, { useState } from "react";
+import Navbar from "../Navbar";
+import Sidebar from "../Sidebar";
+
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./HeroElements";
 
 function Hero() {
-    return (
-        <div>
-            <HeroContainer>
-                <Navbar />
-                <HeroContent>
-                    <HeroItems>
-                        <HeroH1>
-                            Greatest Burger Ever
-                        </HeroH1>
-                        <HeroP>Ready in 60 seconds</HeroP>
-                        <HeroBtn>Place Order</HeroBtn>
-                    </HeroItems>
-                </HeroContent>
-            </HeroContainer>
-        </div>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <HeroContainer>
+        <Navbar toggle={toggle} />
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+        <HeroContent>
+          <HeroItems>
+            <HeroH1>Greatest Burger Ever</HeroH1>
+            <HeroP>Ready in 60 seconds</HeroP>
+            <HeroBtn>Place Order</HeroBtn>
+          </HeroItems>
+        </HeroContent>
+      </HeroContainer>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
